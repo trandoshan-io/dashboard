4484d5a1df2ce1b09711162652da1e5aca4d4c7b
Aloïs Micard <lunamicard@gmail.com>
2019-09-15 19:23:42 +0200

    Add page id

--- src/app/page/page.service.ts
+++ src/app/page/page.service.ts
@@ -5,6 +5,7 @@ import {HttpClient, HttpParams} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 
 export interface SearchResult {
+  id: string;
   url: string;
   crawlDate: Date;
 }
